ebe9a80bb5ee95222f9f3e283ee017642494df71
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-09 19:35:13 +0530

    appication.java

--- application.java
+++ application.java
@@ -3,3 +3,16 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+# code change by project2
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
+# code end
