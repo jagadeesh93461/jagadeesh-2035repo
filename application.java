27cf2560d2b296e697ff54a4ffc3d7161fead255
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-09 19:29:07 +0530

    new changes

--- application.java
+++ application.java
@@ -3,3 +3,12 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+# code change by project1
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
+# end code
